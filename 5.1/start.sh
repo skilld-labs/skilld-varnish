a9f2202b4a4ac8e5bcfdd8c3784609fe012ea03d
Andy Postnikov <apostnikov@gmail.com>
2017-07-13 03:19:20 +0300

    Add varnish 5.1

---
+++ 5.1/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir -p /var/lib/varnish/`hostname` && chown nobody /var/lib/varnish/`hostname`
+
+sed -i "s/{{ VARNISH_BACKEND_HOST }}/${VARNISH_BACKEND_HOST}/g" /etc/varnish/default.vcl
+sed -i "s/{{ VARNISH_BACKEND_PORT }}/${VARNISH_BACKEND_PORT}/g" /etc/varnish/default.vcl
+sed -i "s/{{ VARNISH_BACKEND_FIRST_BYTE_TIMEOUT }}/${VARNISH_BACKEND_FIRST_BYTE_TIMEOUT}/g" /etc/varnish/default.vcl
+sed -i "s/{{ VARNISH_BACKEND_CONNECT_TIMEOUT }}/${VARNISH_BACKEND_CONNECT_TIMEOUT}/g" /etc/varnish/default.vcl
+sed -i "s/{{ VARNISH_BACKEND_BETWEEN_BYTES_TIMEOUT }}/${VARNISH_BACKEND_BETWEEN_BYTES_TIMEOUT}/g" /etc/varnish/default.vcl
+sed -i "s/{{ VARNISH_ERRORS_TTL }}/${VARNISH_ERRORS_TTL}/g" /etc/varnish/default.vcl
+sed -i "s/{{ VARNISH_GRACE }}/${VARNISH_GRACE}/g" /etc/varnish/default.vcl
+
+varnishd -F -s malloc,${VARNISH_MEMORY} -a :80 -T :81 -f /etc/varnish/default.vcl
