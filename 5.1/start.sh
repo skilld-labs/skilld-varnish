f887548e6b3be8a86edf51438137b483e93c5e61
Alexey Gaydabura <gaydabura@gmail.com>
2017-07-13 16:32:30 +0300

    issue #3 Make purge IP subnet configurable #3

--- 5.1/start.sh
+++ 5.1/start.sh
@@ -2,6 +2,8 @@
 
 mkdir -p /var/lib/varnish/`hostname` && chown nobody /var/lib/varnish/`hostname`
 
+export VARNISH_SUBNET=$(hostname -i)
+
 sed -i "s/{{ VARNISH_BACKEND_HOST }}/${VARNISH_BACKEND_HOST}/g" /etc/varnish/default.vcl
 sed -i "s/{{ VARNISH_BACKEND_PORT }}/${VARNISH_BACKEND_PORT}/g" /etc/varnish/default.vcl
 sed -i "s/{{ VARNISH_BACKEND_FIRST_BYTE_TIMEOUT }}/${VARNISH_BACKEND_FIRST_BYTE_TIMEOUT}/g" /etc/varnish/default.vcl
@@ -9,5 +11,6 @@ sed -i "s/{{ VARNISH_BACKEND_CONNECT_TIMEOUT }}/${VARNISH_BACKEND_CONNECT_TIMEOU
 sed -i "s/{{ VARNISH_BACKEND_BETWEEN_BYTES_TIMEOUT }}/${VARNISH_BACKEND_BETWEEN_BYTES_TIMEOUT}/g" /etc/varnish/default.vcl
 sed -i "s/{{ VARNISH_ERRORS_TTL }}/${VARNISH_ERRORS_TTL}/g" /etc/varnish/default.vcl
 sed -i "s/{{ VARNISH_GRACE }}/${VARNISH_GRACE}/g" /etc/varnish/default.vcl
+sed -i "s/{{ VARNISH_SUBNET }}/${VARNISH_SUBNET}\/12/g" /etc/varnish/default.vcl
 
 varnishd -F -s malloc,${VARNISH_MEMORY} -a :80 -T :81 -f /etc/varnish/default.vcl
