5ba017bd81d977160fd87c375979b69d1bcfcf1c
Andy Postnikov <apostnikov@gmail.com>
2018-11-02 18:34:54 +0200

    Fix 5.2 and add 6.1

---
+++ 6.1/start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mkdir -p /var/lib/varnish/`hostname` && chown nobody /var/lib/varnish/`hostname`
+
+sed -i "s/{{ VARNISH_BACKEND_HOST }}/${VARNISH_BACKEND_HOST}/g" /etc/varnish/default.vcl
+sed -i "s/{{ VARNISH_BACKEND_PORT }}/${VARNISH_BACKEND_PORT}/g" /etc/varnish/default.vcl
+sed -i "s/{{ VARNISH_BACKEND_FIRST_BYTE_TIMEOUT }}/${VARNISH_BACKEND_FIRST_BYTE_TIMEOUT}/g" /etc/varnish/default.vcl
+sed -i "s/{{ VARNISH_BACKEND_CONNECT_TIMEOUT }}/${VARNISH_BACKEND_CONNECT_TIMEOUT}/g" /etc/varnish/default.vcl
+sed -i "s/{{ VARNISH_BACKEND_BETWEEN_BYTES_TIMEOUT }}/${VARNISH_BACKEND_BETWEEN_BYTES_TIMEOUT}/g" /etc/varnish/default.vcl
+sed -i "s/{{ VARNISH_ERRORS_TTL }}/${VARNISH_ERRORS_TTL}/g" /etc/varnish/default.vcl
+sed -i "s/{{ VARNISH_GRACE }}/${VARNISH_GRACE}/g" /etc/varnish/default.vcl
+sed -i "s#{{ VARNISH_SUBNET }}#${VARNISH_SUBNET}#g" /etc/varnish/default.vcl
+sed -i "s#{{ VARNISH_COOKIE_REGEXP }}#${VARNISH_COOKIE_REGEXP}#g" /etc/varnish/default.vcl
+
+varnishd -F -s malloc,${VARNISH_MEMORY} -a :80 -T :81 -p http_resp_hdr_len=${VARNISH_RESPONSE_HEADER_LENGHT} -f /etc/varnish/default.vcl -S ${VARNISH_SECRET_FILE}
--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 NAME = skilldlabs/varnish
-TAGS = 4.1 5.1
+TAGS = 4.1 5.2 6.1
 
 .PHONY: all build push
 
